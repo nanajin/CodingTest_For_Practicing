a83244ffa53d6b94e101571e64e35742ad776364
MiJin Na <nmj5182@gmail.com>
2023-03-21 18:23:40 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -0,0 +1,8 @@
+from itertools import *
+def solution(number):
+    answer = 0
+    l = list(combinations(number,3))
+    for i in l:
+        if sum(i) == 0:
+            answer += 1
+    return answer
