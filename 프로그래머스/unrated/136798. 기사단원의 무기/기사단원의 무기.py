f9e753f5a5b32d1ae7008083844897fa9ba5e423
MiJin Na <nmj5182@gmail.com>
2023-03-23 23:53:20 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.87 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,20 +1,18 @@
 def yaksu(x):
-    cnt = 0
     l = []
     for i in range(1, int(x**0.5)+1):
         if x % i == 0:
-            if i not in l:
-                l.append(i)
-            if (x // i) not in l:
-                l.append(x // i)
-    return len(l)
+            l.append(i)
+            l.append(x // i)
+    return len(set(l))
 def solution(number, limit, power):
     answer = 0
     yaksu_list = [1]
     for i in range(2, number+1):
         yaksu_list.append(yaksu(i))
-    for i in range(len(yaksu_list)):
-        if yaksu_list[i] > limit:
-            yaksu_list[i] = power
-    
-    return sum(yaksu_list)
+    for i in yaksu_list:
+        if i > limit:
+            answer += power
+        else:
+            answer += i
+    return answer
