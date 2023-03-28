0e1c390ac573ca5c7553e9dbc28899af6c3a9d96
MiJin Na <nmj5182@gmail.com>
2023-03-28 22:16:39 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(1,n+1):
+    if i == n:
+        break
+    else:
+        print(" " * (n-i) + "*" * (2*i-1))
+for i in range(1, n+1):
+    print(" " * (i-1) + "*" * (2*n - 2*i + 1))
