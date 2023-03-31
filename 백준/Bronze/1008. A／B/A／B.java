2c24f5985118ce98b8d01727f164e7531611debe
MiJin Na <nmj5182@gmail.com>
2023-04-01 01:40:25 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+      Scanner sc = new Scanner(System.in);
+      double n1 = sc.nextInt();
+      double n2 = sc.nextInt();
+      System.out.println(n1/n2);
+    }
+}
